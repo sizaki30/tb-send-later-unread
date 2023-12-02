3adc3391f2ba6475576f6a4fd1de8b1ebe917956
Satoru Izaki <sizaki30@gmail.com>
2023-12-02 15:11:24 +0900

    build.sh updated.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #
 # Usage: Execute "sh build.sh" to create an add-on file in the "build" directory.
 #
-zip -r build/tb-send-later-unread-$(git describe --tags).xpi _locales images src manifest.json
+VERSION=$(grep '"version"' manifest.json | tr -d ',' | cut -d ':' -f 2 | tr -d '"' | tr -d '[:space:]')
+zip -r "build/tb-version-${VERSION}.xpi" images mainVersion manifest.json
